d813555295cd9dea53096cbe8c6466bae6c5dcd0
Ereb-999 <Naofumi999@yandex.ru>
2023-10-13 23:10:01 +0400

    metod chapter 1.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-    public static void leapYear(int year){
+    public static void yearLeap(int year){
 
         if (year > 1584 && year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println(year + " год - високостный год.");
@@ -8,37 +8,38 @@ public class Main {
         }
     }
 
-    public static void phoneVersion (short clientOS, short clientDeviceYear){
+    public static void versionPhone (short clientOS, short clientDeviceYear){
 
-        if (clientOS == 1 && clientDeviceYear > 2015) {
+        if (clientOS == 1 && clientDeviceYear > 2023) {
             System.out.println("Установите версию приложения для Android по ссылке.");
         }
 
-        if (clientOS == 0 && clientDeviceYear > 2015) {
+        if (clientOS == 0 && clientDeviceYear > 2023) {
             System.out.println("Установите версию приложения для iOS по ссылке.");
         }
-        if (clientOS == 1 && clientDeviceYear < 2015){
+        if (clientOS == 1 && clientDeviceYear < 2023){
             System.out.println("Установите облегченную версию приложения для Android по ссылке ");
         }
-        if (clientOS == 0 && clientDeviceYear < 2015) {
+        if (clientOS == 0 && clientDeviceYear < 2023) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
         }
     }
 
-    public static void bankCard(short deliveryDistance) {
-
+    public static int cardBank(short deliveryDistance) {
+        int days = 0;
         if (deliveryDistance <= 20) {
-            System.out.println("Потребуеться дней: " + 1);
+            days++;
+            return days;
         }
-        if (deliveryDistance > 20 && deliveryDistance <= 60) {
-            System.out.println("Потребуеться дней: " + 2);
+        if (deliveryDistance <= 60) {
+            days += 2;
+            return days;
         }
-        if (deliveryDistance > 60 && deliveryDistance <= 100) {
-            System.out.println("Потребуеться дней: " + 3);
+        if (deliveryDistance <= 100) {
+            days += 3;
+            return days;
         }
-        if (deliveryDistance > 100) {
-            System.out.println("Доставки нет");
-        }
+        return days;
     }
 
     public static void main(String[] args) {
@@ -46,7 +47,7 @@ public class Main {
         System.out.println("Задача 1");
         int year;
         year = 2021;
-        leapYear(year);
+        yearLeap(year);
 
         System.out.println();
 
@@ -55,14 +56,25 @@ public class Main {
         short clientDeviceYear;
         clientOS = 1;
         clientDeviceYear = 2020;
-        phoneVersion(clientOS, clientDeviceYear);
+        versionPhone(clientOS, clientDeviceYear);
 
         System.out.println();
 
         System.out.println("Задача 3");
-        short deliveryDistance;
-        deliveryDistance = 100;
-        bankCard(deliveryDistance);
+        short deliveryDistance1 = 10;
+        int deliveryDays = cardBank(deliveryDistance1);
+        if (deliveryDays == 0){
+            System.out.println("Расстояние свыше 100 км не предусмотрено доставкой");
+        } else {
+            System.out.println("Потребуеться " + deliveryDays + " дней");
+
+        }
+
+
+
+
+
+
 
 
 
